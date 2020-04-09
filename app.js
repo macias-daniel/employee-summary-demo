facade8692b4dbe63d733ba43481ad983b49fd6e
PinkFished <d.macias.apps@gmail.com>
2020-04-08 20:01:22 -0700

    Refactored code

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const standardQuestions =  [
 const managerQuestions = [
     {
         type: "input",
-        name: "officeNumber",
+        name: "answer",
         message: `What is this employees office number?`
     }
 ]
@@ -70,7 +70,7 @@ const addMoreEmployees = [
         type: "list",
         name: "yesNoAnswer",
         message: "Would you like to add more employees to the site?",
-        choices: [ "Yes","No"]
+        choices: [ "Yes", "No"]
     }
 ]
 
@@ -81,36 +81,40 @@ addEmployee = () => {
         //Defines out what type of employee was selected
         let employeeType = standardAnswers.type.toLowerCase() + "Questions"
     
-        //String references to question variables defined above
+        //String references to question arrays defined above
         const questionRef = {"managerQuestions": managerQuestions, "engineerQuestions": engineerQuestions, "internQuestions": internQuestions}
     
         //Asking employee type specific questions
         inquirer.prompt(questionRef[employeeType]).then( specificAnswers =>{
 
+            //String refenereces to objects required at the top of the code
             const typeRef = {"Manager": Manager, "Engineer": Engineer, "Intern": Intern}
             
             //Create correct employee object and push it to the employee array
             employeeArray.push(new typeRef[standardAnswers.type](standardAnswers.name,  standardAnswers.id, standardAnswers.email, specificAnswers.answer))
     
-            //Ask the user if they would like to add more employees if yes loop through everything again else return
+            //Ask the user if they would like to add more employees if yes loop through everything again otherwise return and create html file
             inquirer.prompt(addMoreEmployees).then(response =>{
+
                 if(response.yesNoAnswer === "Yes"){ 
                     console.log("")
                     console.log("=========================================")
                     console.log("")
                     addEmployee()
                 }else{
+
                     fs.writeFile("./output/log.html", render(employeeArray), err => {
                         if(err){
                             return console.log(err)
                         }
                         console.log("Your file has been created")
                     });
-  
-                }   
+                } 
+
             })
 
         })
+
     })
 }
 
